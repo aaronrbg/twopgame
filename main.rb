a965b58e1915426d66f2807f36c8513bedaade23
aaronrbg <aaronrbg@aaronrbg.com>
2018-12-03 18:26:03 -0800

    creates player class

---
+++ main.rb
@@ -0,0 +1,23 @@
+#require modules
+require './player'
+
+player1 = Player.new('Player1')
+player2 = Player.new('Player2')
+
+p player1
+p player2
+
+#create players
+#create the game
+
+#game loop
+    #create a question
+    #ask for input
+    #compare answer
+    #lose_life?
+    #check if game over?
+    #ouput scores
+    #switch players
+
+#win output
+
